92c2b2f118edd08d3747959ae5bf307df09fb3cf
reMKTR Deploy <deploy@remktr.com>
2026-06-26 23:39:47 +0000

    Initial deploy - reMKTR website fully self-contained

---
+++ hs/scriptloader/50136663.js
@@ -0,0 +1,11 @@
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+var _hsp = window._hsp = window._hsp || [];
+_hsp.push(['addEnabledFeatureGates', []]);
+_hsp.push(['setBusinessUnitId', 0]);
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js-na2.hs-banner.com/v2/50136663/banner.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-50136663",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":50136663,"data-hsjs-env":"prod","data-hsjs-hublet":"na2"});
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js-na2.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-50136663",0,{"crossorigin":"anonymous","data-leadin-portal-id":50136663,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":50136663,"data-hsjs-env":"prod","data-hsjs-hublet":"na2"});
+var _hsq = window._hsq = window._hsq || [];
+_hsq.push(['setTrackingGate', 'AnalyticsTracking:ReferrerQueryParamMerging']);
+_hsq.push(['setTrackingGate', 'AnalyticsTracking:InitialUrlQueryParamMerging']);
+!function(e,t){if(!document.getElementById(e)){window.__hsReferrer=window.__hsReferrer||document.referrer;window["__hsInitialUrl"]=window["__hsInitialUrl"]||window.location.href;var c=document.createElement("script");c.src="https://js-na2.hs-analytics.net/analytics/1782498900000/50136663.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
